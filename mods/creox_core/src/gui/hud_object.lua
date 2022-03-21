ca498e70a291e8b6ffe4a1a573f1fc7853dcdae9
TehnoTheDragon <tehnothedragon@gmail.com>
2022-03-21 21:56:11 +0300

    Update hud_object.lua

--- mods/creox_core/src/gui/hud_object.lua
+++ mods/creox_core/src/gui/hud_object.lua
@@ -1,3 +1,32 @@
 local HudObject = class.extend("HudObject")
 creox.gui.HudObject = HudObject
 
+function HudObject:init(player, element_type)
+    self._player = player
+    self._id = player:add_hud({
+        hud_elem_type = element_type;
+    })
+    self._values = {}
+end
+
+function HudObject:set(key, value)
+    self._values[key] = value
+    return self
+end
+
+function HudObject:uset(key, value)
+    self._values[key] = value
+    self:update()
+    return self
+end
+
+function HudObject:update()
+    for key, value in pairs(self._values) do
+        self._player:hud_change(self._id, key, value)
+    end
+    return self
+end
+
+function HudObject:delete()
+    self._player:hud_remove(self._id)
+end
