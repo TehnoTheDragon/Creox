c56336153f78a2c21c9472d7f8fc784f3eeb5677
TehnoTheDragon <tehnothedragon@gmail.com>
2022-04-04 18:50:54 +0300

    Update hotbar_gui.lua

--- mods/creox_game/src/player/hotbar_gui.lua
+++ mods/creox_game/src/player/hotbar_gui.lua
@@ -3,7 +3,7 @@ local Player = creox.player.Player
 minetest.register_on_joinplayer(function(playerInstance, last_login)
     --local username = playerInstance:get_player_name()
     --local player = Player.get_by_username(playerInstance)
-
+    
     playerInstance:hud_set_hotbar_image("hotbar_slots.png")
     playerInstance:hud_set_hotbar_selected_image("hotbar_selected_slot.png")
 end)
