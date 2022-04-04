412af9d280e613384cbd5e6636a1ca73982f9949
TehnoTheDragon <tehnothedragon@gmail.com>
2022-04-04 18:50:49 +0300

    Update init.lua

--- mods/creox_core/init.lua
+++ mods/creox_core/init.lua
@@ -12,10 +12,26 @@ function creox.mod_path(mod_name)
     return minetest.get_modpath(mod_name)
 end
 
+function creox.world_path()
+    return minetest.get_worldpath()
+end
+
 function creox.dofile(filepath)
     filepath = filepath:gsub("/", "\\")
     return dofile(creox.mod_path() .. "\\" .. filepath)
 end
+
+function creox.empty_function()
+    return function() end
+end
+
+--> Get an insecure environment
+
+local insecure_environment = minetest.request_insecure_environment()
+if not insecure_environment then
+    minetest.log("Some features of creox_core can be unavailable. add \"" .. creox.mod_name() .. "\" mod to secures.trusted_mods in the settings if you want that everything work properly")
+end
+IE = insecure_environment
 
 --> Doings stuff with vendors
 
@@ -55,6 +71,8 @@ creox.dofile("src/player/player.lua")
 creox.backend = {}
 creox.dofile("src/backend/mod_storage.lua")
 creox.dofile("src/backend/datastore.lua")
+creox.dofile("src/backend/command_builder.lua")
+creox.dofile("src/backend/privilege.lua")
 
 -- src/math/...
 creox.dofile("src/math/vector3.lua")
@@ -62,3 +80,6 @@ creox.dofile("src/math/vector3.lua")
 -- src/gui/...
 creox.gui = {}
 creox.dofile("src/gui/formspec.lua")
+
+IE = nil
+insecure_environment = nil
